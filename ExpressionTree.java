adcf57ee4b79a60df7153ab950b28a016d0f192d
nmontgo <89201522+nmontgo@users.noreply.github.com>
2023-02-26 19:15:45 -0800

    Add files via upload

---
+++ ExpressionTree.java
@@ -0,0 +1,22 @@
+package model;
+
+public class ExpressionTree {
+	private boolean myContainsRoot;
+	
+	public void makeEmpty() {
+		
+	}
+	
+	public void printTree() {
+		
+	}
+	
+	@Override
+	public String toString() {
+		
+	}
+	
+	public void evaluate(final Spreadsheet theSpreadsheet) {
+		
+	}
+}
