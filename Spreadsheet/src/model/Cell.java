0dd6cd3f95d423a0bb15bb96c77e2ee3b1b91a0c
nmontgo <89201522+nmontgo@users.noreply.github.com>
2023-02-26 19:56:20 -0800

    The code will compile now.

---
+++ Spreadsheet/src/model/Cell.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Cell {
+	private String myFormula;
+	
+	private Integer myValue;
+	
+	private ExpressionTree myExpressionTree;
+	
+	public void setFormula (final String theFormula) {
+		myFormula = theFormula;
+	}
+	
+	public int evaluate() {
+		return 0;
+	}
+}
