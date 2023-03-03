0832fc69faf76c711cea49b085345d291a0e9ace
montg <montgomerynathameion@gmail.com>
2023-03-02 22:52:11 -0800

    Moved the isOperator method to the ExpressionTreeNode class.

--- Spreadsheet/src/model/ExpressionTreeNode.java
+++ Spreadsheet/src/model/ExpressionTreeNode.java
@@ -1,6 +1,11 @@
 package model.Spreadsheet.src.model;
 
 public class ExpressionTreeNode {
+	private final static char Plus = '+';
+	private final static char Minus = '-';
+	private final static char Mult = '*';
+	private final static char Div = '/';
+	private final static char LeftParen = '(';
 	public ExpressionTreeNode left;
 
 	public ExpressionTreeNode right;
@@ -13,5 +18,19 @@ public class ExpressionTreeNode {
 
 	public void setToken(final Token theToken) {
 		myToken = theToken;
+	}
+
+	/**
+	 * Return true if the char ch is an operator of a formula.
+	 * Current operators are: +, -, *, /, (.
+	 * @param ch  a char
+	 * @return  whether ch is an operator
+	 */
+	boolean isOperator (char ch) {
+		return ((ch == Plus) ||
+				(ch == Minus) ||
+				(ch == Mult) ||
+				(ch == Div) ||
+				(ch == LeftParen) );
 	}
 }
--- Spreadsheet/src/model/SpreadSheetUtility.java
+++ Spreadsheet/src/model/SpreadSheetUtility.java
@@ -26,19 +26,7 @@ public class SpreadSheetUtility {
         }
         return returnString;
     }
-    /**
-     * Return true if the char ch is an operator of a formula.
-     * Current operators are: +, -, *, /, (.
-     * @param ch  a char
-     * @return  whether ch is an operator
-     */
-    boolean isOperator (char ch) {
-        return ((ch == Plus) ||
-                (ch == Minus) ||
-                (ch == Mult) ||
-                (ch == Div) ||
-                (ch == LeftParen) );
-    }
+
     /**
      * Given an operator, return its priority.
      *
